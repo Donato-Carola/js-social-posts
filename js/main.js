c8ec22fc01e8b035eb721fb2a191a924a92c2008
Donato-Carola <don94@live.it>
2023-11-28 17:55:24 +0100

    ricostruzione dei post usando una funzione per vedere di lavorare sul colore del mi piace per darlo a tutti non solo al primo

--- js/main.js
+++ js/main.js
@@ -59,42 +59,15 @@ const posts = [
 
 const divContainer=document.querySelector('div.posts-list');
 
-posts.forEach((element) => {
-    divContainer.innerHTML += ` 
-    <div class="post">
-    <div class="post__header">
-        <div class="post-meta">                    
-            <div class="post-meta__icon">
-                <img class="profile-pic" src=${element.author.image} alt="${element.author.name}'s image">                    
-            </div>
-            <div class="post-meta__data">
-                <div class="post-meta__author">${element.author.name}</div>
-                <div class="post-meta__time">${element.created}</div>
-            </div>                    
-        </div>
-    </div>
-    <div class="post__text">${element.content}</div>
-    <div class="post__image">
-        <img src=${element.media} alt="">
-    </div>
-    <div class="post__footer">
-        <div class="likes js-likes">
-            <div class="likes__cta">
-                <a class="like-button  js-like-button" href="#" data-postid="1">
-                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                    <span class="like-button__label">Mi Piace</span>
-                </a>
-            </div>
-            <div class="likes__counter">
-                Piace a <b id="like-counter-1" class="js-likes-counter">${element.likes}</b> persone
-            </div>
-        </div> 
-    </div>            
-    </div>
-    `
+posts.forEach((element,index) => {
+    divContainer.innerHTML += generatePost (element.author.image, element.author.name, element.created, element.content, element.media, element.likes, index)
     
+   
+
 
 })
+
+
 
 
 const buttonLike = document.querySelector('a.like-button');
@@ -102,6 +75,46 @@ const buttonLike = document.querySelector('a.like-button');
 
 buttonLike.addEventListener('click', function(){
     buttonLike.classList.toggle('like-button--liked')
+
+
     
 })
 
+
+//Function 
+
+function generatePost(imgSrc,name,dateGenerate,content,media,likes,index){
+    return ` 
+    <div class="post">
+    <div class="post__header">
+        <div class="post-meta">                    
+            <div class="post-meta__icon">
+                <img class="profile-pic" src=${imgSrc} alt="${name}'s image">                    
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">${name}</div>
+                <div class="post-meta__time">${dateGenerate}</div>
+            </div>                    
+        </div>
+    </div>
+    <div class="post__text">${content}</div>
+    <div class="post__image">
+        <img src=${media} alt="">
+    </div>
+    <div class="post__footer">
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button  js-like-button" href="#" data-postid="${index}">
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter">
+                Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
+            </div>
+        </div> 
+    </div>            
+    </div>
+    `
+
+}
